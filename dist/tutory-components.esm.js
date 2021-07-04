c06f9b81b06314f6f6ffffc28816d0677390c3d8
stalyn <stalyn.alcocer@idrica.com>
2021-07-04 22:00:28 +0200

    .

---
+++ dist/tutory-components.esm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+var SimpleButton = function SimpleButton() {
+  return React.createElement("div", null, React.createElement("span", null, "Hello Simple Button"));
+};
+
+export { SimpleButton };
+//# sourceMappingURL=tutory-components.esm.js.map
